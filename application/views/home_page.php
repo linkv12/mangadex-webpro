343594776ca9612328e3b12f1de59dc280a30a8a
linkv12 <thehellsingingslicer@gmail.com>
2019-04-12 18:17:29 +0700

    add fav icon

---
+++ application/views/home_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MangaDex</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <!-- for FF, Chrome, Opera -->
+
+    <!--<link rel="icon" type="image/png" href="</?= base_url('/assets/favicons/favicon-16x16.png')?>" sizes="16x16">-->
+    <link rel="shortcut icon" type="image/png" href="<?= base_url('/assets/favicons/favicon-32x32.png')?>" sizes="32x32">
+    <!-- for IE -->
+    <!--<link rel="icon" type="image/x-icon" href="<//?= base_url('/assets/favicons/favicon.ico')?>">-->
+    <!--<link rel="shortcut icon" type="image/x-icon" href="<//?= base_url('/assets/favicons/favicon.ico')?>"/>-->
+  </head>
+  <body>
+    <?php echo 'fuck yu men</br>' ?>
+    <?php echo base_url('/assets/favicons/favicon-32x32.png') ?>
+  </body>
+</html>
