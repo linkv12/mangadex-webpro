0e7e61d68823a3374eb9bb3447aaa95380156cc0
linkv12 <thehellsingingslicer@gmail.com>
2019-04-12 17:41:58 +0700

    add sql [Not final] Pending review

---
+++ application/views/test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Code Igniter View Example</title>
+  </head>
+  <body>
+      CodeIgniter View Example
+  </body>
+</html>
